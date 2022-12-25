ad61440ee23e599bce58f429a90fe3d7d37439ca
simon <simonbreen567@gmail.com>
2022-12-25 01:37:12 +0000

    Add audio file test when button cliked & add background images to buttons

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
 
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-  alert ("I got clicked!")
+    var audio = new Audio("assets/sounds/tom-1.mp3");
+    audio.play();
 });
 
 }

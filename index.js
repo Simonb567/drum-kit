5273edb0cc95e97467da536143d1bbbfa499467d
simon <simonbreen567@gmail.com>
2022-12-25 02:34:20 +0000

    Test 'this' Javascript property by using the querySelector to change the font color of the button that triggered the eventListener

--- index.js
+++ index.js
@@ -3,8 +3,13 @@
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
 
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-    var audio = new Audio("assets/sounds/tom-1.mp3");
-    audio.play();
+
+    this.style.color = "white";
 });
 
 }
+
+
+
+// var audio = new Audio("assets/sounds/tom-1.mp3");
+// audio.play();

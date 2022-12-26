3056468e4d93d813fda513b4d84af7e8c2daa7af
simon <simonbreen567@gmail.com>
2022-12-26 21:23:53 +0000

    Add animation on screen through setTimeout function when button are clicked on screen or on keyboard

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
 
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
   });
 }
 
 
 document.addEventListener("keydown",function(event) {
   makeSound(event.key);
+  buttonAnimation(event.key);
 });
 
 
@@ -54,4 +56,14 @@ function makeSound(key) {
     default: console.log(buttonInnerHTML);
 
   }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("."+currentKey);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  },100);
+
 }
